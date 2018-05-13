28db36af49103fc6580937685e859c7a4bba537d
Segun <segunolalive@gmail.com>
2018-05-13 11:20:47 +0100

    configure css modules

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from 'Styles/character.scss';
 
 
 const Character = (props) => (
-  <div>
+  <div className={styles.character}>
     <img src={props.image} alt={props.name} />
     <h3>Name: {props.name}</h3>
     <p>Status: {props.status}</p>
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 
 module.exports = {
+  resolve: {
+    extensions: [".js", ".json"],
+    alias: {
+      Styles: path.resolve(__dirname, 'src/styles'),
+    }
+  },
   module: {
     rules: [
       {
@@ -24,9 +31,19 @@ module.exports = {
         test: /\.(s)?css$/,
         use: [
           process.env.NODE_ENV !== 'production' ? 'style-loader' :
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
+            MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]",
+            }
+          },
+          {
+            loader: "sass-loader",
+          },
+
         ]
       }
     ]
